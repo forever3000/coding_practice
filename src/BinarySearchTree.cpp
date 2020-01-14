183650ac549dcb7204ff50f2c8fd09b7dc60d192
forever3000 <n.congvu@gmail.com>
2020-01-15 05:31:33 +0900

    Update BinarySearchTree.cpp
    Support Level Order Traversal

--- src/BinarySearchTree.cpp
+++ src/BinarySearchTree.cpp
@@ -2,6 +2,7 @@
 // only missing number in a sorted array of 
 // distinct elements within limited range. 
 #include <iostream> 
+#include <queue>
 using namespace std; 
   
 class BST {
@@ -24,6 +25,18 @@ class BST {
 
         // In order traversal
         void InOderTraversal(BST *);
+
+        // Height of tree
+        int Height(BST *);
+
+        // Print Given Level
+        void PrintGivenLevel(BST *, int);
+
+        // Level Order Traversal
+        void LevelOrderTraversal(BST *);
+
+        // Level Order Traverlal by queue
+        void LevelOrderTraverlsalByQueue(BST *);
 };
 
 BST::BST():data(0), left(NULL), right(NULL){};
@@ -70,6 +83,64 @@ void BST::InOderTraversal(BST *root) {
     InOderTraversal(root->right);
 }
 
+int BST::Height(BST *root) {
+    if (root == NULL) {
+        return 0;
+    } else {
+        int lheight = Height(root->left);
+        int rheight = Height(root->right);
+        return max(lheight, rheight) + 1;
+    }
+}
+
+void BST::PrintGivenLevel(BST *root, int level) {
+    if (root == NULL) {
+        return;
+    } else {
+        if (level == 1) {
+            cout << root->data << " ";
+        } else {
+            PrintGivenLevel(root->left, level - 1);
+            PrintGivenLevel(root->right, level - 1);
+        }
+    }
+}
+
+void BST::LevelOrderTraversal(BST *root) {
+    if (root == NULL) {
+        return;
+    } else {
+        int height = Height(root);
+
+        for (int i = 1; i <= height; i++) {
+            PrintGivenLevel(root, i);
+        }
+    }
+}
+
+void BST::LevelOrderTraverlsalByQueue(BST *root) {
+    if (root == NULL) {
+        return;
+    }
+
+    queue<BST *> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        BST *node = q.front();
+        cout << node->data << " ";
+        q.pop();
+
+        if (node->left != NULL) {
+            q.push(node->left);
+        }
+
+        if (node->right != NULL) {
+            q.push(node->right);
+        }
+    }
+}
+
 int main (void) {
     BST b;
     BST *root = NULL;
@@ -83,10 +154,13 @@ int main (void) {
     b.Insert(root, 36);
     b.Insert(root, 45);
     b.Insert(root, 76);
-    b.InOderTraversal(root);
-    b.Mirror(root);
-    cout << " After traversal: " << endl;
-    b.InOderTraversal(root);
+    b.Insert(root, 70);
+    b.Insert(root, 80);
+    
+    cout << b.Height(root) << endl;
+    b.LevelOrderTraversal(root);
+    cout << "Level Order Traversal by queue: " << endl;
+    b.LevelOrderTraverlsalByQueue(root);
 
     return 0;
 }
